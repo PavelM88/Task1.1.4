18d6d3a8732c01fd42adc9bcd6071ecd6ef2de4c
Pavel Mamaev <mamai_88@mail.ru>
2021-06-16 11:51:46 +0300

    Catch exceptions in dao and call rollback method.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -21,64 +21,96 @@ public class UserDaoHibernateImpl implements UserDao {
                 "name VARCHAR(20) NOT NULL," +
                 " lastName VARCHAR(30) NOT NULL," +
                 "age TINYINT UNSIGNED NOT NULL);";
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             Query query = session.createSQLQuery(sql);
             query.executeUpdate();
             transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void dropUsersTable() {
         String sql = "DROP TABLE IF exists users";
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             Query query = session.createSQLQuery(sql);
             query.executeUpdate();
             transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = new User(name, lastName, age);
             user.setId((Long) session.save(user));
             session.save(user);
             transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
         String sql = "DELETE FROM users WHERE id = " + id;
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             Query query = session.createSQLQuery(sql);
             query.executeUpdate();
             transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try(Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            Query query = session.createQuery("FROM User");
+        Transaction transaction = null;
+        Query query = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            query = session.createQuery("FROM User");
             return query.getResultList();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
+        return query.getResultList();
     }
 
     @Override
     public void cleanUsersTable() {
         String sql = "DELETE FROM users";
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             Query query = session.createSQLQuery(sql);
             query.executeUpdate();
             transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
     }
 }
